347ba4abfcaddad93ddf4eaef4b906ddc3132201
Raphael Ferreira <raphaelferreira.dev@gmail.com>
2022-05-07 19:51:24 -0300

    feat(server): implemented dependecy inversion
    Created repository, use-cases to apply D principle from SOLID on submit a feedback.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import nodemailer from "nodemailer";
 
-import { prisma } from "./prisma";
+import { PrimaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -17,12 +18,15 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
+  const prismaFeedbacksRepository = new PrimaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
   });
 
   await transport.sendMail({
@@ -37,5 +41,5 @@ routes.post("/feedbacks", async (req, res) => {
     ].join("\n"),
   });
 
-  return res.status(201).json({ data: feedback });
+  return res.status(201).send();
 });
