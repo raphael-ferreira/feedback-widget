dd30592ad09ddecef92a6bc72eb76feb019489d2
Raphael Ferreira <raphaelferreira.dev@gmail.com>
2022-05-07 00:36:49 -0300

    feat: added feedback type buttons

--- web/src/components/Widget/WidgetForm.tsx
+++ web/src/components/Widget/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "../Common/CloseButton";
 
+import bugImageUrl from "../../assets/imgs/bug.svg";
+import ideaImageUrl from "../../assets/imgs/idea.svg";
+import thoughtImageUrl from "../../assets/imgs/thought.svg";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,6 +36,21 @@ export function WidgetForm() {
 
         <CloseButton title="Fechar formulário de feedback" />
       </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Desenvolvido por{" "}
