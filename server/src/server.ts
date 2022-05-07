fdf0c66968f31e42b3fd8bafa4592419cac737d8
Raphael Ferreira <raphaelferreira.dev@gmail.com>
2022-05-07 18:44:07 -0300

    feat(server): added nodemailer config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import nodemailer from "nodemailer";
 
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Feedback Support <support@feedback.com>",
+    to: "Raphael Ferreira <raphaelferreira.dev@gmail.com>",
+    subject: "New feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
