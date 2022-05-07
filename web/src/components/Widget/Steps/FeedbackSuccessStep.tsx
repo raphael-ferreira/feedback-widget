55f8298af615bdaf0ffec2a1517157cd56175da9
Raphael Ferreira <raphaelferreira.dev@gmail.com>
2022-05-07 04:14:25 -0300

    fix: removed duplicated class names

--- web/src/components/Widget/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/Widget/Steps/FeedbackSuccessStep.tsx
@@ -22,7 +22,7 @@ export function FeedbackSuccessStep({
         <button
           type="button"
           onClick={onFeedbackReset}
-          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50"
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50"
         >
           Quero enviar outro
         </button>
